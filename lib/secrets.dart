03dab517dc327c9c5f4835b02dc74967aa55ecc7
CodifyWithShravan <shravankumarthudi777@gmail.com>
2025-10-17 11:01:12 +0530

    removed secrets

---
+++ lib/secrets.dart
@@ -0,0 +1,8 @@
+const String witAiServerToken = '62TOKPXQPHG76X62S377GKLQWW3GL4LS';
+const String openWeatherApiKey = '5d53e414af8392f55e4ba0639b925625';
+const String roboflowApiKey = 'hBcIkEggpG7W928Kh8jm';
+const String roboflowModelEndpoint =
+    'https://detect.roboflow.com/plant-disease-lfscz/2';
+// AIzaSyB71Jpz99o4re6o-uCXzM4JvBmmJl6buSM
+const String geminiApiKey = 'AIzaSyB71Jpz99o4re6o-uCXzM4JvBmmJl6buSM';
+// AIzaSyB71Jpz99o4re6o-uCXzM4JvBmmJl6buSM
